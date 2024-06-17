d16b5745f1f13797955f5ccf1ddf44a20caa0ca4
Chendada-8474 <raymond96383@gmail.com>
2024-06-18 00:25:01 +0800

    feat(highlight): init highlight page

--- src/views/pagesdev/Highlights.vue
+++ src/views/pagesdev/Highlights.vue
@@ -1,9 +1,23 @@
 <template>
     <div className="card">
         <h5>精選</h5>
-        <p>頁面製作中...</p>
+        <p>搜尋條件</p>
+
     </div>
 </template>
 <script setup>
+import { ref, onMounted } from 'vue'
+
+import { getMedia } from '../../service/Media'
+import { getBehaviors } from '../../service/Behaviors'
+import { getMembers } from '../../service/Members'
+import { getSpecies } from '../../service/Species'
+
+
+const members = ref([])
+const species = ref([])
+const behariors = ref([])
+const media = ref([])
+
 
 </script>
