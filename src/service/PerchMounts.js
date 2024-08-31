5844c99405495f39b4c69acb60b67055bc4a4275
Chendada-8474 <raymond96383@gmail.com>
2024-08-31 23:42:30 +0800

    feat(perch mount): merged monthly table

--- src/service/PerchMounts.js
+++ src/service/PerchMounts.js
@@ -61,5 +61,55 @@ export function cancelClaimPerchMount(perchMountID) {
 
 export function getMonthlyPendingByPerchMountID(perchMountID) {
     var endPoint = new URL(`${window.BACKEND_HOST}/perch_mounts/${perchMountID}/monthly_pending/`)
+    var counts = fetchWithloginCheck(endPoint.href, "GET")
     return fetchWithloginCheck(endPoint.href, "GET")
 }
+
+function yearMonth(year, month) {
+    return year * 100 + month
+}
+
+export function mergeCountTables(emptyCounts, detectedCounts, compeletedCounts) {
+    var monthYearMap = {}
+
+    for (const counts of [emptyCounts, detectedCounts, compeletedCounts]) {
+
+        for (const count of counts) {
+            monthYearMap[yearMonth(count.year, count.month)] = {
+                emptyCounts: 0,
+                detectedCounts: 0,
+                compeletedCounts: 0,
+                year: count.year,
+                month: count.month,
+            }
+        }
+    }
+
+    for (const count of emptyCounts) {
+        var ym = yearMonth(count.year, count.month)
+        monthYearMap[ym].emptyCounts = count.count
+
+    }
+    for (const count of detectedCounts) {
+        var ym = yearMonth(count.year, count.month)
+        monthYearMap[ym].detectedCounts = count.count
+    }
+    for (const count of compeletedCounts) {
+        var ym = yearMonth(count.year, count.month)
+        monthYearMap[ym].compeletedCounts = count.count
+    }
+
+    var counts = []
+
+    for (const [k, v] of Object.entries(monthYearMap)) {
+        v.yearMonth = k
+        counts.push(v)
+    }
+
+    counts.sort(function (a, b) {
+        return a - b
+    });
+
+    return counts
+}
+
